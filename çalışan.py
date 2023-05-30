889c9753db95508d04f0800a36542f1eedf5a143
nurullah hilcan <128026157+nrlhhlcn@users.noreply.github.com>
2023-05-30 14:38:35 +0300

    Update and rename çalışan.py to çalışan.py
    zam hakki eklendi

--- çalışan.py
+++ çalışan.py
@@ -17,7 +17,29 @@ class Calisan(Insan):
         return self.__maas
     def set_maas(self,maas):
         self.__maas=maas
-    
+    def zam_hakki(self):
+        try:
+            if self.get_tecrube()<2:
+                print("Tecrübeniz iki yıldan az olduğu için yeni maaşınız hesaplanmadi")
+                return 0
+
+            elif 2<=self.get_tecrube()<=4  and self.get_maas()<15000:
+                zam_hakki=self.get_maas()%self.get_tecrube()
+                yeni_maas=self.get_maas() +(self.get_maas()*zam_hakki)/100
+
+                return yeni_maas
+
+            elif self.get_tecrube()>4 and self.get_maas()<25000:
+                zam_hakki = (self.get_maas() % self.get_tecrube())/2
+                yeni_maas = self.get_maas() + (self.get_maas() * zam_hakki) / 100
+
+                return yeni_maas
+
+            else:
+                return "hatalili giris"
+
+        except ValueError:
+            print("Hatalı veri girişi!")
 
     def __str__(self):
         return f"--------------BİLGİLER--------------------\n" \
