d3c477e84e794bd6bf84737a646c6671c7d4c908
nurullah hilcan <128026157+nrlhhlcn@users.noreply.github.com>
2023-05-30 15:04:23 +0300

    Update ISSIZ.py
    statü bul eklendi

--- ISSIZ.py
+++ ISSIZ.py
@@ -10,6 +10,31 @@ class Issiz(Insan):
     def set_tecrube(self,tecrube):
         self.__tecrube=tecrube
         self.statu=self.statü_bul()
+        
+     def statü_bul(self):
+        maks_etki = 0
+        karar_statü = ""
+        for statü, yıl in self.__tecrube.items():
+            try:
+                yıl = int(yıl)
+
+                if statü == "Mavi Yaka":
+                    etki = yıl / 5
+                elif statü == "Meyaz Yaka":
+                    etki = yıl * (35 / 100)
+                elif statü == "yönetici":
+                    etki = yıl * (45 / 100)
+
+                if etki > maks_etki:
+                    maks_etki = etki
+                    karar_statü = statü
+            except:
+                return "Geçersiz bir yıl veya statü, lütfen tekrar deneyiniz."
+
+        if karar_statü == "":
+            return "Bilinmiyor ne olduğu"
+        else:
+            return karar_statü
 
     
 
