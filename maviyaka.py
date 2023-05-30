4cb4e9a76c8092f2c026add68c00d4a0af0083e6
nurullah hilcan <128026157+nrlhhlcn@users.noreply.github.com>
2023-05-30 14:54:53 +0300

    Update maviyaka.py
    açıklama satırı eklendi

--- maviyaka.py
+++ maviyaka.py
@@ -1,17 +1,18 @@
-from Calisan import Calisan
+from Calisan import Calisan #kütüphaneyi dahil ediyoruz
 
-class MaviYaka(Calisan):
+class MaviYaka(Calisan):#maviyaka adında sınıf oluşturuyoruz
 
     def __init__(self,isim,soyisim,yas,cinsiyet,uyruk,tc_numarasi,sektor,tecrube,maas,yipranma_payi):
         super().__init__(isim,soyisim,yas,cinsiyet,uyruk,tc_numarasi,sektor,tecrube,maas)
+                ##super().__init__ methodu bizim calışan sınıfından bilgileri almamızı sağlıyor
         self.__yipranma_payi=yipranma_payi
 
-    def get_yipranma_payi(self):
+    def get_yipranma_payi(self): #bu fonsiyon bize yipranma payinini döndürecek
         return self.__yipranma_payi
-    def set_yipranma_payi(self,yipranma_payi):
+    def set_yipranma_payi(self,yipranma_payi):#bu fonsiyon yeni yıpranma payini eski yipranma payi ile değiştirmemizi sağlıyor
         self.__yipranma_payi=yipranma_payi
         
-    def Zam_hakki(self):
+    def Zam_hakki(self):#bu fonksiyon çalışanın tecrübe, maaş ve yipranma payına göre yeni maaşını hesaplamamızı sağlıyor
 
         if 2<=self.get_tecrube()<=4 and self.get_maas()<15000:
             zam_hakki=(self.get_maas()%self.get_tecrube())/2+(self.get_yipranma_payi()*10)
@@ -27,7 +28,7 @@ class MaviYaka(Calisan):
 
             return yeni_maas
 
-    def __str__(self): 
+    def __str__(self): #str methodu ile kullanıcını bilgilerini konsol  ekranını yazdırıyoruz.....
         return f"--------------BİLGİLER--------------------\n" \
                f"isim:{self.get_isim()}\nsoyisim:{self.get_soyisim()}\nTecrübe:{self.get_tecrube()}\nYeni Maaş:{self.get_maas()+self.Zam_hakki()}"
 
