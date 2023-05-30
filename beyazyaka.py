a712869a8d16b69aa79ff80aa38d175d76e88cf1
nurullah hilcan <128026157+nrlhhlcn@users.noreply.github.com>
2023-05-30 14:46:47 +0300

    Update beyazyaka.py
    zam hakki eklendi

--- beyazyaka.py
+++ beyazyaka.py
@@ -12,7 +12,16 @@ class Beyazyaka(Calisan):
     def set_tesvik_primi(self,tesvik_primi):
         self.__tesvik_primi=tesvik_primi
 
+     def Zam_hakki(self):
+        if 2<=self.get_tecrube()<=4 and self.get_maas()<15000:
+            zam_hakki=(self.get_maas()%self.get_tecrube())*5+self.get_tesvik_primi()
 
+            return zam_hakki
+        elif self.get_tecrube()>4 and self.get_maas() <25000:
+            zam_hakki=(self.get_maas()%self.get_tecrube())*4+self.get_tesvik_primi()
+            return zam_hakki
+        
+     
     def __str__(self): 
         return f"--------------BİLGİLER--------------------\n" \
                f"isim:{self.get_isim()}\nsoyisim:{self.get_soyisim()}\nTecrübe:{self.get_tecrube()}\nYeni Maaş:{self.get_maas()+self.Zam_hakki()}"
