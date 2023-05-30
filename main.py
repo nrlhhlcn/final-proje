149f25261bfd99dee3fb0a9c1511e4bc54f06a95
nurullah hilcan <128026157+nrlhhlcn@users.noreply.github.com>
2023-05-30 14:57:23 +0300

    Create main.py
    kütüphaneleri dahil etme ve nesneleri oluşturma

---
+++ main.py
@@ -0,0 +1,31 @@
+from Insan import Insan
+from Issiz import Issiz
+from Calisan import Calisan     #KÜTÜPHANELERİ VE MODÜLLERİ DAHİL EDİYORUZ....!!!
+from MaviYaka import MaviYaka
+from BeyazYaka import Beyazyaka
+import pandas as pd
+
+
+                        #NESNELERİ OLUŞTURUYORUZ!!!
+insan1=Insan("Nurullah","Hilcan",20,"Erkek👨","Türk",123456789)
+insan2=Insan("Sude","Aktürk",20,"kız👩","Türk",987654321)
+
+issiz1=Issiz("dwayne"," johnson",51,"erkek👨","ABD",987654321,7)
+issiz2=Issiz("Angelina","Jolie",47,"kız👩","BD",12345678,4)
+issiz3=Issiz("Hande","Erçel","29","kız👩","türk",12345678,3)
+
+
+calisan1=Calisan("Murat","Boz",43,"erkek👨","Türk",123456789,"diğer",3,12000)
+calisan2=Calisan("Elon","Musk",32,"Erkek👨","Güney Amerike",123456789,"Teknoloji💻",8,22000)
+calisan3=Calisan("scarlett","johansson",38,"kız👩","ABD",123456789,"inşaat🏢",8,23000)
+
+
+
+maviYaka1=MaviYaka("Demet","Mutlu",42,"Kız👩","Türk",987654321,"teknoloji💻",4,14000,0.2)
+maviYaka2=MaviYaka("Acun","ılıcalı",53,"Erkek👨","Türk",123456789,"diğer",4,13000,0.5)
+maviYaka3=MaviYaka("lionel","messi",53,"Erkek👨","arjantin",123456789,"diğer",4,11000,0.5)
+
+
+beyazYaka1=Beyazyaka("dominic","toretto",55,"erkek👨","ABD",123456789,"diğer",5,14500,500)
+beyazYaka2=Beyazyaka("tony","stark",55,"erkek👨","ABD",123456789,"muhasebe🧾",3,13000,1500)
+beyazYaka3=Beyazyaka("fahriye","evcen",37,"kız👩","türk",9876543254,"muhasebe🧾",4,12000,2000)
